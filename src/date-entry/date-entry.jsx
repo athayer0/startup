90ba3b14b4563e4cb51954c7144a2845557e1a49
athayer0 <aaronsthayer@gmail.com>
2025-10-08 22:42:47 -0600

    added jsx stub files

---
+++ src/date-entry/date-entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function DateEntry() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>date entry displayed here</div>
+    </main>
+  );
+}
---
+++ src/saved-events/saved-events.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function SavedEvents() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>saved events displayed here</div>
+    </main>
+  );
+}
