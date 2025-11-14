9f7bca404efbffd74ca42a16dc59e1fd5c4178ca
athayer0 <aaronsthayer@gmail.com>
2025-11-13 22:23:50 -0700

    added database.js

---
+++ service/database.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('missionary');
+const userCollection = db.collection('user');
+const timelineCollection = db.collection('timeline');
+const savedEventsCollection = db.collection('savedEvents');
+
